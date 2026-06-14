645c6c44453c85ef043e25201e2bafac262c7e20
liss22 <liss22@hanmail.net>
2026-06-15 01:39:13 +0900

    빌드 옵션 수정

--- build-wasm/compile_wasm.sh
+++ build-wasm/compile_wasm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source "/opt/emsdk/emsdk_env.sh"
+
 emcc ../src/platform/wasm/wasm-main.c libmgba.a \
     -I../include \
     -I../src \
@@ -12,11 +13,13 @@ emcc ../src/platform/wasm/wasm-main.c libmgba.a \
     -DM_CORE_GBA \
     -pthread \
     -s WASM=1 \
+    -s USE_PTHREADS=1 \
+    -s PTHREAD_POOL_SIZE=4 \
     -s MODULARIZE=1 \
     -s EXPORT_NAME='createMGBA' \
-    -s EXPORTED_FUNCTIONS='["_mgba_init","_mgba_load_rom","_mgba_run_player","_mgba_post_frame","_mgba_get_video_buffer","_mgba_get_width","_mgba_get_height","_mgba_set_button","_mgba_save_state","_mgba_load_state","_mgba_free_buffer","_mgba_get_audio_samples","_mgba_get_audio_sample_rate","_mgba_set_volume","_malloc","_free"]' \
-    -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap","HEAP8","HEAPU8","HEAP16","HEAP32"]' \
+    -s INITIAL_MEMORY=128MB \
     -s ALLOW_MEMORY_GROWTH=1 \
-    -s PTHREAD_POOL_SIZE=4 \
+    -s EXPORTED_FUNCTIONS='["_mgba_init","_mgba_load_rom","_mgba_run_player","_mgba_post_frame","_mgba_get_video_buffer","_mgba_get_width","_mgba_get_height","_mgba_set_button","_mgba_save_state","_mgba_load_state","_mgba_free_buffer","_mgba_get_audio_samples","_mgba_get_audio_sample_rate","_mgba_set_volume","_malloc","_free"]' \
+    -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap","HEAP8","HEAPU8","HEAP16","HEAP32","setValue","getValue"]' \
     -O3 \
     -o mgba.js
